e4c1dcc09be1bdcc4d2cc134249a9d674918c54c
k-lindberg <kristin.n.lindberg@gmail.com>
2026-02-03 19:32:53 -0700

    week 5 learning activity inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Minnie Mouse", "Fractions");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Mickey Mouse", "Division", "12", "1-20");
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Donald Duck", "History", "Revolutionary War");
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
     }
 }
